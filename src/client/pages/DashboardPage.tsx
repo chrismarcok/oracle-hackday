351b5df45831631ab414c97826dd35080f85fb33
Adam Zartin <adam.zartin@oracle.com>
2020-05-28 13:24:38 -0400

    search bar

--- src/client/pages/DashboardPage.tsx
+++ src/client/pages/DashboardPage.tsx
@@ -1,13 +1,18 @@
 import React, { useEffect, useState } from "react";
-import {posts} from "../data/dummydata/posts"
-import {PostComp} from "../comps/dashboard/Post";
+import { posts } from "../data/dummydata/posts"
+import { PostComp } from "../comps/dashboard/Post";
 import { Footer } from "../comps/ojet/Footer";
+import { Button, Form, FormGroup, Label, Input, FormText } from 'reactstrap';
+import {Post} from "../data/post"
+
 
 interface DashboardPageProps {}
 
 export const DashboardPage: React.FC<DashboardPageProps> = ({}) => {
 
-  const [fetched, setFetched] = useState([]);
+  const [fetched, setFetched] = useState<Post[]>([]);
+  const [searchQuery, setsearchQuery] = useState("");
+
 
   useEffect(() => {
     fetch("/api/post")
@@ -17,7 +22,7 @@ export const DashboardPage: React.FC<DashboardPageProps> = ({}) => {
       } else {
         throw new Error("error");
       }
-    }).then((data:any[]) => {
+    }).then((data:Post[]) => {
       console.log(data);
       setFetched(data);
     }).catch((err:any) => {
@@ -25,14 +30,28 @@ export const DashboardPage: React.FC<DashboardPageProps> = ({}) => {
     })
   }, [])
 
+  let searchOnChange = function(e) { 
+    setsearchQuery(`${e.target.value}`)   
+  };
+
 
   return (
     <>
       <div className="dashboard">
         <div className="dashboard-outermost">
           <div className="dashboard-container">
+
+
+            <div>
+              <Input type="text" name="search" id="search" placeholder="search some stuff" onChange={searchOnChange}/>
+            </div>
+
               {
-                fetched.map((post, index) => {
+                fetched.filter( post => {
+                  console.log(searchQuery)
+                  return post.title.toLowerCase().includes(searchQuery.toLowerCase()) || post.author.name.toLowerCase().includes(searchQuery.toLowerCase())
+                }
+                  ).map((post, index) => {
                     return (
                     <PostComp 
                         post={post} 
