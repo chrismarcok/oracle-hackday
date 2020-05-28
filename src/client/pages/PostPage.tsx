a8900970889f296cebd042f2778ebb61dc4fb3dd
Adam Zartin <adam.zartin@oracle.com>
2020-05-28 11:39:41 -0400

    connected post page with backend

--- src/client/pages/PostPage.tsx
+++ src/client/pages/PostPage.tsx
@@ -1,9 +1,9 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
 import { Container, Row, Col } from 'reactstrap';
 import { CommentComp } from '../comps/Comment'
 import {comments} from "../data/dummydata/comments"
 import {useParams} from "react-router-dom";
-import {posts} from "../data/dummydata/posts"
+import {Post} from "../data/post"
 import {PostComp} from "../comps/dashboard/Post";
 
 
@@ -12,16 +12,35 @@ interface PostPageProps {
 }
 
 export const PostPage: React.FC<PostPageProps> = ({}) => {
-        let {postID} = useParams()
+        const {postID} = useParams()
+
+        const [fetched, setFetched] = useState<Post>(null);
+
+        useEffect(() => {
+          fetch(`/api/post/${postID}`)
+          .then((res: any) => {
+            if (res.status === 200){
+              return res.json()
+            } else {
+              throw new Error("error");
+            }
+          }).then((data:Post) => {
+            console.log(data);
+            setFetched(data);
+          }).catch((err:any) => {
+            console.log(err);
+          })
+        }, [])
 
         return (
         <Container style={{paddingTop:"50px"}}>
             <Row >
-               <PostComp post={posts[0]}/>
+               {fetched !== null && <PostComp post={fetched}/>}
             </Row>
             <Row style={{minHeight:"auto", width: "90%", backgroundColor: "white", marginTop: "20px",  height: "100vh", display:"block"}}>
             {
-                comments.map((comment, index) => {
+                fetched !== null &&
+                fetched.comments.map((comment, index) => {
                     return (
                         <div  key={index}style={{ marginTop:"20px"}}>
                              <CommentComp {... comment} />
