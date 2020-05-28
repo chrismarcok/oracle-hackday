ebdaff57915f69b8d96844e6cbd2b63891e367ab
Chris Marcok <chris.marcok@oracle.com>
2020-05-28 18:49:00 -0400

    repo in the post page now

--- src/client/pages/PostPage.tsx
+++ src/client/pages/PostPage.tsx
@@ -6,6 +6,7 @@ import { Post, Comment } from "../data/post";
 import { PostComp } from "../comps/dashboard/Post";
 var markdown = require("markdown").markdown;
 import axios from "axios";
+import { formatDate } from "../utils/utils";
 
 interface PostPageProps {}
 
@@ -29,15 +30,14 @@ export const PostPage: React.FC<PostPageProps> = ({}) => {
   const [addingComment, setAddingComment] = useState<boolean>(false);
   const [repoCommit, setRepoCommit] = useState<Commit>(null);
   const [repoInfo, setRepoInfo] = useState<RepoInfo>(null);
+  const [commitAvatar, setCommitAvatar] = useState<string>(null)
   let token = "";
 
-  
   useEffect(() => {
-    axios.get("/token")
-      .then((res:any) => {
-        console.log('%cTOKEN','color:red;font-family:system-ui;font-size:4rem;-webkit-text-stroke: 1px black;font-weight:bold');
-        console.log(res);
-        if (!res.data.error){
+    axios
+      .get("/token")
+      .then((res: any) => {
+        if (!res.data.error) {
           token = res.data.token;
         }
         return fetch(`/api/post/${postID}`);
@@ -52,56 +52,110 @@ export const PostPage: React.FC<PostPageProps> = ({}) => {
       .then((data: Post) => {
         console.log(data);
         setFetched(data);
-        if (data.repo && token !== ""){
-          const promises = [axios.get(`https://orahub.oci.oraclecorp.com/api/v4/projects/${data.repo.repo_id}/repository/branches/${data.repo.branch}?private_token=${token}`)]
-          promises.push(axios.get(`https://orahub.oci.oraclecorp.com/api/v4/projects/${data.repo.repo_id}?private_token=${token}`))
+        if (data.repo && token !== "") {
+          const promises = [
+            axios.get(
+              `https://orahub.oci.oraclecorp.com/api/v4/projects/${data.repo.repo_id}/repository/branches/${data.repo.branch}?private_token=${token}`
+            ),
+          ];
+          promises.push(
+            axios.get(
+              `https://orahub.oci.oraclecorp.com/api/v4/projects/${data.repo.repo_id}?private_token=${token}`
+            )
+          );
           return Promise.all(promises);
         } else {
-          throw new Error("No repo found.")
+          throw new Error("No repo found.");
         }
       })
       .then((values: any[]) => {
-        console.log('%cHey, Listen!','color:red;font-family:system-ui;font-size:4rem;-webkit-text-stroke: 1px black;font-weight:bold');
-        console.log(values);
-        values.forEach((v:any) => {
-          
-          if (v.config.url.includes("branches")){
+        let username;
+        values.forEach((v: any) => {
+          if (v.config.url.includes("branches")) {
             setRepoCommit({
-              author:v.data.commit.author_name,
-              date:v.data.commit.committed_date,
-              message:v.data.commit.message,
-            })
+              author: v.data.commit.author_name,
+              date: v.data.commit.committed_date,
+              message: v.data.commit.message,
+            });
+            username = v.data.commit.author_name;
           } else {
             setRepoInfo({
               description: v.data.description,
               name: v.data.name,
               url: v.data.web_url,
-            })
+            });
           }
-        })
+        });
+
+        return axios.get(
+          `https://orahub.oci.oraclecorp.com/api/v4/users?username=${username}`
+        );
+      })
+      .then((res: any) => {
+        setCommitAvatar(res.data[0].avatar_url)
       })
       .catch((err: any) => {
         console.log(err);
       });
   }, []);
 
-
   return (
     <>
       <Container style={{ paddingTop: "50px" }}>
         <Row>{fetched !== null && <PostComp post={fetched} large />}</Row>
-        {(fetched && fetched.repo && repoInfo && repoCommit) && <div style={{
-          width: "100%",
-          backgroundColor: "red",
-          marginBottom: "10px",
-        }}>
-          done!
-        </div>}
+        {fetched &&
+          fetched.repo &&
+          repoInfo &&
+          repoCommit &&
+          commitAvatar && (
+            <div
+              style={{
+                width: "100%",
+                backgroundColor: "#e6e3e0",
+                marginBottom: "10px",
+                padding: "10px",
+              }}
+            >
+              <p style={{ fontSize: "0.8rem" }}>
+                The OP has included a relevant OraHub repository:
+              </p>
+              <a href={repoInfo.url} target="_blank">
+                <h2>
+                  {repoInfo.name}:{fetched.repo.branch}
+                </h2>
+              </a>
+              <p style={{ fontStyle: "italic" }}>"{repoInfo.description}"</p>
+
+              <br />
+              <div>
+                <div
+                  style={{
+                    height: "50px",
+                    width: "50px",
+                    backgroundImage: `url(${commitAvatar})`,
+                    backgroundSize: "contain",
+                    display: "inline-block",
+                    marginRight: "10px"  
+                  }}
+                />
+                <div style={{
+                  display: "inline-block"
+                }}>
+                  <h5>
+                    Last Commit by {repoCommit.author} on{" "}
+                    {formatDate(new Date(repoCommit.date).getTime())}
+                  </h5>
+                  <p style={{ fontStyle: "italic" }}>"{repoCommit.message}"</p>
+                </div>
+              </div>
+            </div>
+          )}
         <div
           style={{
             backgroundColor: "#e6e3e0",
             color: "black",
             padding: "10px",
+            cursor: "pointer",
           }}
           onClick={() => {
             setAddingComment(!addingComment);
